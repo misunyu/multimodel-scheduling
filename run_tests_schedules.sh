65288d854c3fde558c1624843fb01f97590e1e3c
msyu <misunyu@gmail.com>
2025-11-10 09:55:54 +0900

    Reduce schedule execution duration from 30 to 10 seconds in test scripts

--- run_tests_schedules.sh
+++ run_tests_schedules.sh
@@ -86,16 +86,16 @@ invoke_one() {
 
   if [[ -x "$ROOT_DIR/schedule_executor_main.sh" ]]; then
     if (( TIMEOUT_SECS > 0 )); then
-      timeout "$TIMEOUT_SECS" bash -c '"$0" -schedule "$1" --duration 30 --auto_start_all' "$ROOT_DIR/schedule_executor_main.sh" "$schedule_file"
+      timeout "$TIMEOUT_SECS" bash -c '"$0" -schedule "$1" --duration 10 --auto_start_all' "$ROOT_DIR/schedule_executor_main.sh" "$schedule_file"
     else
-      "$ROOT_DIR/schedule_executor_main.sh" -schedule "$schedule_file" --duration 30 --auto_start_all
+      "$ROOT_DIR/schedule_executor_main.sh" -schedule "$schedule_file" --duration 10 --auto_start_all
     fi
   else
     # Direct python invocation without sudo
     if (( TIMEOUT_SECS > 0 )); then
-      timeout "$TIMEOUT_SECS" "$PY" "$ROOT_DIR/schedule_executor_main.py" --schedule "$schedule_file" --duration 30 --auto_start_all
+      timeout "$TIMEOUT_SECS" "$PY" "$ROOT_DIR/schedule_executor_main.py" --schedule "$schedule_file" --duration 10 --auto_start_all
     else
-      "$PY" "$ROOT_DIR/schedule_executor_main.py" --schedule "$schedule_file" --duration 30 --auto_start_all
+      "$PY" "$ROOT_DIR/schedule_executor_main.py" --schedule "$schedule_file" --duration 10 --auto_start_all
     fi
   fi
 
