4ac28d66fc10c85f60ecc2c9ad3940cc3f038dd7
msyu <misunyu@gmail.com>
2025-12-01 20:52:38 +0900

    Update legend to replace NPU2 references with NPU0 in `schedule_generator_app.py`

--- schedule_generator_app.py
+++ schedule_generator_app.py
@@ -131,8 +131,8 @@ class ONNXProfilerApp(QMainWindow):
         self.legend_label = QLabel()
         self.legend_label.setText(
             "<span style='background-color:#cce6ff;'>&nbsp;&nbsp;&nbsp;</span> CPU &nbsp;&nbsp;"
-            "<span style='background-color:#ffffcc;'>&nbsp;&nbsp;&nbsp;</span> use NPU1 &nbsp;&nbsp;"
-            "<span style='background-color:#ffd699;'>&nbsp;&nbsp;&nbsp;</span> use NPU2"
+            "<span style='background-color:#ffffcc;'>&nbsp;&nbsp;&nbsp;</span> use NPU0 &nbsp;&nbsp;"
+            "<span style='background-color:#ffd699;'>&nbsp;&nbsp;&nbsp;</span> use NPU1"
         )
         self.legend_label.setStyleSheet("font-size: 12px; padding: 2px;")
         
